bdd68b05d2f6adeae1f5a705c1c85effa302169b
Artiom Kalugin <a.o.kalugin@protonmail.com>
2020-12-24 08:32:05 +0000

    Initial commit.

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+    mode: "development",
+    entry: path.resolve(__dirname, "src/index.js"),
+    output: {
+        path: path.resolve(__dirname, "build"),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"]
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: ["style-loader", "css-loader", "sass-loader"]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "src/index.html")
+        })
+    ],
+    resolve: {
+        extensions: [".js", ".jsx"]
+    }
+}
